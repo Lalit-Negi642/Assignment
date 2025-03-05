704643c0751325bdf9b3dcfc7b502aa94e4e6119
Lalit Negi <lalitnegi.160610.com>
2025-03-05 21:23:01 +0530

    Hello

--- src/assignment/Demo.java
+++ src/assignment/Demo.java
@@ -1,10 +1,12 @@
 package assignment;
 
-public class Assignment1_DataTypes {
+public class Demo {
 
 	public static void main(String[] args) {
 		
-    
+		System.out.println("Hello, World");
+		
+
 	}
 
 }
